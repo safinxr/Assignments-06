33a097aa7180353926c0424d7abab5f6a551afed
Safinxr <safinxr@gmail.com>
2022-03-01 23:44:28 +0600

    others adding

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ document.getElementById("search-button").addEventListener('click', function(){
         .then(data =>phoneSearch(data))
     }
 })
-
+// Search Result area 🍕🍔🍟🌭🍿🍟🍔🍕
 const phoneSearch = (data) =>{
     const mainDiv =document.getElementById("mainDiv");
-    if(data.status ===false){
+    if(data.status === false){
         mainDiv.innerHTML=`<h3 class="mx-auto text-center text-muted">No result found <i class="fa-solid fa-face-sad-tear"></i></h3>`
     }
     else{
@@ -39,13 +39,14 @@ const phoneSearch = (data) =>{
     }
     }
 }
-
+// Details button area 🍑🍑🍑🍑🍑🍑🍑
 const detailsButton =(details)=>{
     fetch(`https://openapi.programming-hero.com/api/phone/${details}`)
     .then(res => res.json())
     .then(data =>phoneDetails(data.data))
 }
 
+// phone details area📱📱📱📱📱📱📱
 const phoneDetails =(data) =>{
     const leftArea =document.getElementById("left-area");
     const rightArea =document.getElementById("right-area");
@@ -53,21 +54,42 @@ const phoneDetails =(data) =>{
     data.mainFeatures.sensors.forEach(element => {
         sensors=element+','+sensors;
     });
+    let releaseDate ="";
+    if(data.releaseDate === ""){
+        releaseDate ="Releas date not found"
+    }
+    else{
+        releaseDate =data.releaseDate;
+    }
+    console.log(data.others);
+    console.log(data);
+    let others ="";
+    if(data.others === undefined){
+        console.log("alu")
+    }
+    else{
+        
+    }
+
     leftArea.innerHTML=`
-    <img class="w-75 h-auto ms-5" src="${data.image}"> 
+        <img class="w-75 h-auto ms-5" src="${data.image}"> 
     `
     rightArea.innerHTML=`
-    <p><span class="span-width">Name</span> : ${data.name}</p>
-    <p><span class="span-width">ReleaseDate</span> : ${data.releaseDate}</p>
-    <p><span class="span-width">Storage</span> : ${data.mainFeatures.storage}</p>
-    <p><span class="span-width">DisplaySize</span> : ${data.mainFeatures.displaySize}</p>
-    <p><span class="span-width">ChipSet</span> : ${data.mainFeatures.chipSet}</p>
-    <p><span class="span-width">Memory</span> : ${data.mainFeatures.memory}</p>
-    <p><span class="span-width">Sensors</span> : ${sensors}</p>
-    
-    
+        <p><span class="span-width">Name</span> : ${data.name}</p>
+        <p><span class="span-width">ReleaseDate</span> : ${releaseDate}</p>
+        <p><span class="span-width">Storage</span> : ${data.mainFeatures.storage}</p>
+        <p><span class="span-width">DisplaySize</span> : ${data.mainFeatures.displaySize}</p>
+        <p><span class="span-width">ChipSet</span> : ${data.mainFeatures.chipSet}</p>
+        <p><span class="span-width">Memory</span> : ${data.mainFeatures.memory}</p>
+        <p><span class="span-width">Sensors</span> : ${sensors}</p>
+        <p><span class="span-width">WLAN</span> : ${data.others?.WLAN}</p>
+        <p><span class="span-width">Bluetooth</span> : ${data.others?.Bluetooth}</p>
+        <p><span class="span-width">GPS</span> : ${data.others?.GPS}</p>
+        <p><span class="span-width">NFC</span> : ${data.others?.NFC}</p>   
+        <p><span class="span-width">Radio</span> : ${data.others?.Radio}</p>
+        <p><span class="span-width">USB</span> : ${data.others?.USB}</p>
+        
 
     `
-    console.log(data)
 
 }   
